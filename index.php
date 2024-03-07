3b74e01fc9960488c0eb5b494f6c8fbef0120d2d
Hüdayi Emre Sarıça <150180409+Dark-des@users.noreply.github.com>
2024-03-07 10:52:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -80,8 +80,49 @@
 // $ortalama = ($vize*0.4) + ($final*0.6);
 //echo  "{$ad} isimli ögrencinin vize notu {$vize},final notu {$final} ve  ortalaması ", $ortalama , "dür";
 
+# Operatörler
+ 
+# Aritmetik Operatörler 
 
+# PHPde matematik işlemi yapmak için kullanılır.
+ 
+//$a = 2;
+//$b = 6; 
+//$c = 5;
+//$sonuc = $a + $b * $c;
+//echo $sonuc;
 
+#Atama Operatörü 
+#matematiksel ifadelerin daha kısa yazılmalarını sağlayan operatörlerdir.Böylece kodlar daha düzenli görünür ve kod kısa yazılmış olur.
 
+//$a= 10 ;
+//$b= 12 ;
+//$c= 15 ;
+//$d= 20 ;
 
+ #b ve a verisine birleştirme operatörü kullanarak a ve b yi birleştirme. 
+//$birlesik = $a.=$b;
+//echo $birlesik; 
+ #b ve a verisine  atama operatörü kullanarak a ve b nin toplamını bulma.
+//$toplam = $b+=$a;
+//echo $toplam;
+
+#Arttırma / azaltma  operatörleri 
+
+//$a = 2;
+//$b = 6 ;
+//$c = ++$a + $b++ - --$a + ($b+$a--);
+//echo "$a $b $c";
+# yukardaki kodun çıktısı nedir ?
+#1716 
+
+#Karşılaştırma Operatörleri 
+# Operatörlerin sağındaki ve solundaki değerlerin,öperatörlerin belirli ifadeyi sağlayıp sağlamadığını kontrol eder.Eğer yanlış ise "false" dögru ise "true ifadesini döndürür. 
+ 
+//$a = 2;
+//$b = 6;
+//$c = 3;
+//$d= '3';
+//$karsılastir= $a == $b;
+//echo $karsılastir; 
 ?>
